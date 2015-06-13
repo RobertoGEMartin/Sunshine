522894a7706504eece5f90d42e9016dbe3cb2238
@rgem52 <rgomez@hi-iberia.es>
2015-06-14 00:10:50 +0200

    JSON Parsing

--- app/src/main/java/com/robertsolutions/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/robertsolutions/sunshine/MainActivityFragment.java
@@ -1,9 +1,7 @@
 package com.robertsolutions.sunshine;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,12 +11,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,18 +21,28 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String TAG = "MyActivity";
+
     public MainActivityFragment() {
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //You should place your menu items in to menu.
+        //onCreateOptionsMenu >> For this method to be called, you must have first called
+        setHasOptionsMenu(true);
+
+    }
+
     public View rootView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView =  inflater.inflate(R.layout.fragment_main, container, false);
-        //You should place your menu items in to menu.
-        //onCreateOptionsMenu >> For this method to be called, you must have first called
-        setHasOptionsMenu(true);
+
 
         String[] forecastArray = {
                 "Today - Sunny - 30º/33º",
@@ -87,84 +89,9 @@ public class MainActivityFragment extends Fragment {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        return true;
 
-        // Handle item selection
-        switch (id) {
-            case R.id.action_refresh:
-                //TODO
-                return true;
-            case R.id.action_settings:
-                //TODO
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
-    public class FecthWeatherTask extends AsyncTask<Void, Void, Void>{
 
-        private final String LOG_TAG = FecthWeatherTask.class.getSimpleName();
-        @Override
-        protected Void doInBackground(Void... params) {
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
-                    }
-                }
-            }
-            return null;
-        }
-    }
 }
